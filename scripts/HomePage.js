843f91e5299d7efae32aa042274332e54e81cfc8
Sanmi Akinwunmi <sanmi.hq@gmail.com>
2023-07-03 09:44:02 +0100

    modified speedometer component

--- scripts/HomePage.js
+++ scripts/HomePage.js
@@ -1,77 +1,28 @@
-// JavaScript code (HomePage.js)
+// Get the current location of the driver.
+let previousLocation;
 
-document.addEventListener("DOMContentLoaded", function () {
-  const arrowWrapper = document.getElementById("arrowWrapper");
-  let previousPosition = null;
-
-  function updateSpeedometer(speed) {
-    const rotation = 90 - (speed / 120) * 180; // Adjust the formula based on your speedometer's range
-    arrowWrapper.style.transform = `rotate(${rotation}deg)`;
-  }
-
-  function calculateSpeed(position) {
-    if (!previousPosition) {
-      previousPosition = position;
-      return 0;
-    }
-
-    const distance = calculateDistance(
-      previousPosition.coords.latitude,
-      previousPosition.coords.longitude,
-      position.coords.latitude,
-      position.coords.longitude
+const calculateSpeed = (location) => {
+  if (previousLocation) {
+    const distance = Math.sqrt(
+      (previousLocation.coords.latitude - location.coords.latitude) ** 2 +
+      (previousLocation.coords.longitude - location.coords.longitude) ** 2
     );
 
-    const timeDiff = (position.timestamp - previousPosition.timestamp) / 1000; // Convert milliseconds to seconds
-    const speed = distance / timeDiff;
-    previousPosition = position;
+    const time = location.timestamp - previousLocation.timestamp;
+    const speed = distance / time;
 
-    return speed;
+    // Update the element with the new speed.
+    document.getElementById("speed").innerHTML = speed.toFixed(2);
   }
 
-  function calculateDistance(lat1, lon1, lat2, lon2) {
-    const earthRadius = 6371; // Radius of the earth in kilometers
-    const dLat = degreesToRadians(lat2 - lat1);
-    const dLon = degreesToRadians(lon2 - lon1);
+  // Update the previous location with the current location.
+  previousLocation = location;
+};
 
-    const a =
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(degreesToRadians(lat1)) *
-        Math.cos(degreesToRadians(lat2)) *
-        Math.sin(dLon / 2) *
-        Math.sin(dLon / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const distance = earthRadius * c;
+// Set up a watch to continuously monitor the position.
+const watchId = navigator.geolocation.watchPosition(calculateSpeed);
 
-    return distance;
-  }
-
-  function degreesToRadians(degrees) {
-    return (degrees * Math.PI) / 180;
-  }
-
-  function handlePosition(position) {
-    const speed = calculateSpeed(position);
-    updateSpeedometer(speed);
-  }
-
-  function handlePositionError(error) {
-    console.error(error);
-  }
-
-  function startGPS() {
-    if ("geolocation" in navigator) {
-      const options = { enableHighAccuracy: true };
-      navigator.geolocation.watchPosition(
-        handlePosition,
-        handlePositionError,
-        options
-      );
-    } else {
-      console.error("Geolocation is not supported");
-    }
-  }
-
-  const startButton = document.getElementById("startButton");
-  startButton.addEventListener("click", startGPS);
-});
+// Stop the watch after a certain duration (e.g., 10 minutes).
+setTimeout(() => {
+  navigator.geolocation.clearWatch(watchId);
+}, 10 * 60 * 1000); // Stop after 10 minutes (adjust as needed)
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyAjIW7Upo-Fg8FpuIPv6k-HG3_LvxPweQw",
+  authDomain: "test-project-53a38.firebaseapp.com",
+  projectId: "test-project-53a38",
+  storageBucket: "test-project-53a38.appspot.com",
+  messagingSenderId: "945205431071",
+  appId: "1:945205431071:web:dc5e009673b6627f667e2a",
+  measurementId: "G-N5FRSM4XJ9",
+});
